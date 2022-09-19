4540554a83a6c4a6ce66c5cf0508c55190793b01
Rohan <rrchakrabarty@gmail.com>
2022-09-18 20:42:50 -0400

    Update landing-page.js

--- js/landing-page.js
+++ js/landing-page.js
@@ -56,7 +56,7 @@ particlesJS('particles-js',
       "image": {
         "src": "img/github.svg",
         "width": 100,
-        "height": 100
+        "height": 50
       }
     },
     "opacity": {
